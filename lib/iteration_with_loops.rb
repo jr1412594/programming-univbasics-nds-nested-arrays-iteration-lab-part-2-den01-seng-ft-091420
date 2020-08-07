a6e1e644de63b4a55b5a8720d6044937b165f1fc
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-07 22:26:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,8 @@ while row_index < src.count do
   element_index = 0 
   low_temp = 150 
   while element_index < src[row_index].count do
-    if src[row_index][element_index] < low_temp
-      low_temp = src[row_index][element_index]
+    if low_temp > src[row_index][element_index]
+      p low_temp = src[row_index][element_index]
     end
     element_index += 1 
   end
